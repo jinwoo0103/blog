abd1377505f431f08a2f17b657f380d30129f4bc
jinwoo0103 <jinwoo0103@kaist.ac.kr>
2025-04-28 21:48:40 +0900

    feat: Configure prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,26 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+
+  plugins: [
+    /**
+     * https://github.com/simonhaenisch/prettier-plugin-organize-imports
+     */
+    "prettier-plugin-organize-imports",
+
+    /**
+     * https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
+     *
+     * `prettier-plugin-tailwindcss` must be loaded last.
+     * (not always, but in this case it's due to above `prettier-plugin-organize-imports`)
+     * check: https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
+     */
+    "prettier-plugin-tailwindcss",
+  ],
+};
+
+export default config;
